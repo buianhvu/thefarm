dc85ee8555ebae0ac369d803028ab7a1e2497430
Bui Anh Vu <Bui Anh Vu>
2017-05-18 08:59:35 +0700

    avc

--- site/widget/sidebar.php
+++ site/widget/sidebar.php
@@ -24,10 +24,10 @@
                           <span>CATTLE</span>
                       </a>
                       <ul class="sub">
-                          <li><a  href="index.php?action=list/buffalo_list">Buffalo</a></li>
-                          <li><a  href="index.php?action=list/chicken_list">Chicken</a></li>
-                          <li><a  href="index.php?action=list/cow_list">Cow</a></li>
-                          <li><a  href="index.php?action=list/pig_list">Pig</a></li>
+                          <li><a  href="index.php?action=buffalo_list">Buffalo</a></li>
+                          <li><a  href="index.php?action=chicken_list">Chicken</a></li>
+                          <li><a  href="index.php?action=cow_list">Cow</a></li>
+                          <li><a  href="index.php?action=pig_list">Pig</a></li>
                       </ul>
                   </li>
 
